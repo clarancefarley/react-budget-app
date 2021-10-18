96972b40c850fb2c24243c5fa16947c5dea546a4
Clarance Farley <clarancefarley@gmail.com>
2021-10-17 17:39:36 -0700

    update expenseList with data from appContext

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,14 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from '../context/AppContext';
 import ExpenseItem from "./ExpenseItem";
 
 const ExpenseList = () => {
-  const expenses = [
-    { id: 122333, name: "Shopping", cost: 50 },
-    { id: 122333, name: "Holiday", cost: 300 },
-    { id: 122333, name: "Transportation", cost: 70 },
-    { id: 122333, name: "Fuel", cost: 40 },
-    { id: 122333, name: "Child Care", cost: 500 },
-  ];
+  const { expenses } = useContext(AppContext);
+
   return (
     <ul className="list-group">
       {expenses.map((expense) => (
